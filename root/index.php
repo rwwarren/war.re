402f457a26b2037ad654679b4a468560b22a4ae3
Ryan Warren <ryan@war.re>
2015-08-27 22:06:14 -0700

    starting with react

--- root/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo '<html><head><script src="/n/scripts/google.js"></script></head><body>You are being redirected, if that doesn\'t work, click ' .
-        '<a href="/n">here</a> if it does not work</body></html>';
-header("Location: /n");
-exit;
-?>
--- subdomains/ryan/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo 'You are being redirected, if that doesn\'t work, click ' .
-	'<a href="/n">here</a> if it does not work';
-header("Location: /n");
-exit;
-?>

0497e4353dbfb4a9da34812453884f2c017999f3
Ryan Warren <ryan@war.re>
2015-08-29 00:53:57 -0700

    some logos and update

--- inc/Content.js
+++ inc/Content.js
@@ -3,15 +3,25 @@
 
 var Content = React.createClass({
   render: function() {
-      //<div className="content">
+    //<div className="content">
     var now = new Date;
     var theYear = now.getYear();
-    if (theYear < 1900) {
+    if(theYear < 1900) {
       theYear = theYear + 1900
     }
     return (
       <div className="homeContainer">
         <h1>Ryan Warren</h1>
+        <br/>
+        <a href="http://ryan.war.re/n">My Resume</a>
+        <br/>
+        <a href="https://github.com/rwwarren">
+          <img src="/GitHub-Mark-64px.png"/>
+        </a>
+        <br/>
+        <a href="https://linkedin.com/in/ryanwwarren">
+          <img src="/In-Black-66px-R.png"/>
+        </a>
         <p>{'\u00A9'} {theYear} War.re</p>
       </div>
     );

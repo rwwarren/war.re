eea3e0829e0a67484e3b97346d1a2ce31b17440f
Ryan Warren <ryan@war.re>
2015-12-13 23:28:04 -0800

    personal site

--- inc/Content.js
+++ inc/Content.js
@@ -2,7 +2,6 @@
 
 var Content = React.createClass({
   render: function() {
-    //<div className="content">
     var now = new Date;
     var theYear = now.getYear();
     if(theYear < 1900) {
@@ -12,22 +11,35 @@ var Content = React.createClass({
       <div className="homeContainer">
         <h1>Ryan Warren</h1>
         <br/>
+
         <p className="subtitle">
           Software Engineer
         </p>
-        <p>
-          Hi my name is Ryan Warren
+
+        <p className="info">
+          Hi! I am Seattle Java developer currently working for <a href="https://porch.com">Porch.com</a>.
         </p>
+
+        <p className="info">
+          I am currently working on a personal project to learn about react.js among other skills.
+          Click <a href="https://github.com/rwwarren/door-lock">here</a> to check it out.
+        </p>
+
+        <p className="info">
+          If you would like to learn a little more, see my <a href="http://ryan.war.re/n">resume</a>.
+        </p>
+
         <div className="info">
           <a href="http://ryan.war.re/n"></a>
         </div>
         <br/>
-          <a href="https://github.com/rwwarren">
-            <img src="/GitHub-Mark-64px.png"/></a>
-          <a href="https://linkedin.com/in/ryanwwarren">
-            <img src="/In-Black-66px-R.png"/></a>
-          <a href="http://stackoverflow.com/users/1879792/ryan-warren">
-            <img src="/stackoverflow-xxl.png"/></a>
+        <a href="https://github.com/rwwarren">
+          <img src="/GitHub-Mark-64px.png"/></a>
+        <a href="https://linkedin.com/in/ryanwwarren">
+          <img src="/In-Black-66px-R.png"/></a>
+        <a href="http://stackoverflow.com/users/1879792/ryan-warren">
+          <img src="/stackoverflow-xxl.png"/></a>
+
         <p className="text">{'\u00A9'} {theYear} War.re</p>
       </div>
     );

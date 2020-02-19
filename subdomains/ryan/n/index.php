39b1e718899333e6cbca8e4efeec8f520c4c6618
Ryan Warren <ryan.ww@gmail.com>
2020-02-18 22:40:56 -0800

    update wording

--- subdomains/ryan/n/index.php
+++ subdomains/ryan/n/index.php
@@ -216,9 +216,9 @@
             <div class="title"><h3>Software Engineer II, Amazon</h3></div>
             <div class="date">July 2019 - Present</div>
             <ul>
-              <li>Wrote library used to tie ads across the entire ad stack in under 3 milliseconds</li>
-              <li>Mentored interns and junior engineers to help them get hired and promoted</li>
-              <li>Designed and implemented cross team projects that improve monetization, fill rate and latency</li>
+              <li>Built the library used to connect advertisements across the entire stack in under 3 milliseconds</li>
+              <li>Mentor interns and junior engineers to help them achieve their career goals</li>
+              <li>Designed and implemented cross organization projects to improve monetization, fill rate and latency</li>
             </ul>
           </div>
         </li>
